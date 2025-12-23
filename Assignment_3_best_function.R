49baf34481528e21f1bb71cea2bc2ba0c55b9e10
FACSCANTO <ignasipro1@gmail.com>
2025-12-23 09:41:43 +0100

    Incloent l'histograma

--- Assignment_3_best_function.R
+++ Assignment_3_best_function.R
@@ -1,3 +1,18 @@
+# Llegir i importar les dades
+
+outcome_care <- read.csv("C:/Users/ignas/OneDrive/Escritorio/Programming_3/outcome-of-care-measures.csv", colClasses = "character")
+
+View(outcome_care)
+
+
+
+# Histogram of 30 days death rate heart attack
+
+outcome_care_2 <- as.numeric(outcome_care[,11])
+
+hist(outcome_care_2)
+
+
 # Funció best programming assignment 3: 2a versió funcional
 
 best <- function(state, outcome){
@@ -26,4 +41,5 @@ best("NY", 11)
 
 outcome_care$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack[
   outcome_care$Hospital.Name == "GLENDALE ADVENTIST MEDICAL CENTER"
-
+  
+  
